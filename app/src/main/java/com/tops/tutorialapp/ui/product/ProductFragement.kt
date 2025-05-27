907a7da23c7668642f3ca2b5ff07f387df26fd5d
jpCoding003 <jpcoding003@gmail.com>
2025-05-27 15:11:28 +0530

    ..

--- app/src/main/java/com/tops/tutorialapp/ui/product/ProductFragement.kt
+++ app/src/main/java/com/tops/tutorialapp/ui/product/ProductFragement.kt
@@ -3,19 +3,22 @@ package com.tops.tutorialapp.ui.product
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.findNavController
 import com.tops.tutorialapp.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.tops.tutorialapp.databinding.FragmentProductBinding
 
 
 class ProductFragement : Fragment() {
 
-
+private lateinit var binding : FragmentProductBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,7 +28,29 @@ class ProductFragement : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentProductBinding.inflate(inflater,container,false)
+        return binding.root
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product, container, false)
+       // return inflater.inflate(R.layout.fragment_product, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost= requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.dashboard_dawer, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if (menuItem.itemId == R.id.action_add) {
+                    val navController =activity?.findNavController(R.id.nav_host_fragment_content_dashboard_dawer)
+                    navController?.navigate(R.id.action_nav_product_to_nav_new_product)
+                    return true
+                }
+                return  false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
